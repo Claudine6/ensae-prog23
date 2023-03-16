5d194cf62defcaa16f745c8e6ee320ddb4eaf978
cfotsingxmakou <claudine.fotsingmakou@ensae.fr>
2023-03-16 09:53:56 +0000

    dernier jet séances 1 et 2

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,4 +1,4 @@
-from prog import Graph, graph_from_file, find, union, kruskal
+from CMpro import Graph, graph_from_file, find, union, kruskal
 
 
 data_path = "input/"
@@ -7,5 +7,4 @@ file_name = "network.01.in"
 g= graph_from_file("input/network.1.in")
 g_1=kruskal(g)
 print(kruskal(g))
-print(g_1.dfs)
-print(g_1.get_power_and_path(1,9))
+print(g_1.dfs())
